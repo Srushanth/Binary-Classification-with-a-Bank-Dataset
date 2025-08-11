b82546766a28c1cea643c15ebe8f7b06aa3f6eab
Srushanth Baride <SrushanthBaride@Gmail.com>
2025-08-11 16:41:43 +0530

    Formatted code

--- src/sagemaker-pipeline.py
+++ src/sagemaker-pipeline.py
@@ -1,14 +1,15 @@
-"""_summary_
-"""
+"""_summary_"""
 
 import sagemaker
 import numpy as np
 import pandas as pd
 import lightgbm as lgb
 from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
 from sagemaker.workflow.pipeline import Pipeline
 from sagemaker.workflow.function_step import step
 from sagemaker.workflow.step_outputs import get_step
+
 
 @step(display_name="Ingest Data", instance_type="ml.m5.xlarge", instance_count=1)
 def ingest_data(s3_path: str) -> pd.DataFrame:
@@ -23,6 +24,7 @@ def ingest_data(s3_path: str) -> pd.DataFrame:
     train_df = pd.read_csv(s3_path)
     return train_df
 
+
 @step(display_name="Get Feature Column", instance_type="ml.m5.xlarge", instance_count=1)
 def get_feature_column(train_df: pd.DataFrame) -> pd.Series:
     """_summary_
@@ -34,6 +36,7 @@ def get_feature_column(train_df: pd.DataFrame) -> pd.Series:
         pd.Series: _description_
     """
     return train_df["y"]
+
 
 @step(display_name="Pre-process Data", instance_type="ml.m5.xlarge", instance_count=1)
 def preprocess_data(df: pd.DataFrame) -> tuple:
@@ -47,16 +50,16 @@ def preprocess_data(df: pd.DataFrame) -> tuple:
     """
     # Label encoding
     object_labels = [
-        "job", 
-        "marital", 
-        "education", 
-        "default", 
-        "balance", 
-        "housing", 
-        "loan", 
-        "contact", 
-        "month", 
-        "poutcome"
+        "job",
+        "marital",
+        "education",
+        "default",
+        "balance",
+        "housing",
+        "loan",
+        "contact",
+        "month",
+        "poutcome",
     ]
 
     for column_name in object_labels:
@@ -69,8 +72,9 @@ def preprocess_data(df: pd.DataFrame) -> tuple:
 
     return X, y
 
+
 @step(display_name="Split Data", instance_type="ml.m5.xlarge", instance_count=1)
-def split_data(X: pd.DataFrame, y: pd.DataFrame) -> tuple:
+def split_data(x: pd.DataFrame, y: pd.DataFrame) -> tuple:
     """_summary_
 
     Args:
@@ -80,16 +84,19 @@ def split_data(X: pd.DataFrame, y: pd.DataFrame) -> tuple:
     Returns:
         tuple: _description_
     """
-    from sklearn.model_selection import train_test_split
     x_train, x_test, y_train, y_test = train_test_split(
-        X, y, train_size=0.8, random_state=42
+        x, y, train_size=0.8, random_state=42
     )
 
     return x_train, x_test, y_train, y_test
 
+
 @step(display_name="Train Model", instance_type="ml.m5.xlarge", instance_count=1)
 def train_model(
-    x_train: pd.DataFrame, x_test: pd.DataFrame, y_train: pd.DataFrame, y_test: pd.DataFrame
+    x_train: pd.DataFrame,
+    x_test: pd.DataFrame,
+    y_train: pd.DataFrame,
+    y_test: pd.DataFrame,
 ) -> lgb.LGBMClassifier:
     """_summary_
 
@@ -104,8 +111,9 @@ def train_model(
     """
     model = lgb.LGBMClassifier()
     model.fit(x_train, y_train, eval_set=[(x_test, y_test)])
-    
+
     return model
+
 
 @step(display_name="Mae Predictions", instance_type="ml.m5.xlarge", instance_count=1)
 def make_predictions(model: lgb.LGBMClassifier, test_data: pd.DataFrame) -> np.ndarray:
@@ -121,10 +129,20 @@ def make_predictions(model: lgb.LGBMClassifier, test_data: pd.DataFrame) -> np.n
     test_data = test_data.drop(columns=["id"])
     return model.predict_proba(test_data)[:, -1]
 
+
 def create_pipeline():
+    """_summary_
+
+    Returns:
+        _type_: _description_
+    """
     # Define the pipeline steps
-    train_data_path = "s3://srushanth-baride/binary-classification-with-a-bank-dataset/train.csv"
-    test_data_path = "s3://srushanth-baride/binary-classification-with-a-bank-dataset/test.csv"
+    train_data_path = (
+        "s3://srushanth-baride/binary-classification-with-a-bank-dataset/train.csv"
+    )
+    test_data_path = (
+        "s3://srushanth-baride/binary-classification-with-a-bank-dataset/test.csv"
+    )
 
     # Ingest data
     step_ingest_train_data = ingest_data(train_data_path)
@@ -135,21 +153,20 @@ def create_pipeline():
     step_preprocess_test_data = preprocess_data(step_ingest_test_data)
 
     # Split data
-    step_split_train_data = split_data(step_preprocess_train_data[0], step_preprocess_train_data[1])
-    
+    step_split_train_data = split_data(
+        step_preprocess_train_data[0], step_preprocess_train_data[1]
+    )
+
     # Train model
     step_train = train_model(
         step_split_train_data[0],
         step_split_train_data[1],
         step_split_train_data[2],
-        step_split_train_data[3]
+        step_split_train_data[3],
     )
 
     # Make predictions
-    step_predict = make_predictions(
-        step_train,
-        step_preprocess_test_data
-    )
+    step_predict = make_predictions(step_train, step_preprocess_test_data)
 
     # # If you really need explicit dependencies, define them like this:
     # step_preprocess_instance = get_step(step_preprocess)
@@ -165,11 +182,19 @@ def create_pipeline():
     # Create and return pipeline
     pipeline = Pipeline(
         name="BankMarketingPipeline",
-        steps=[step_ingest_train_data, step_preprocess_train_data, step_preprocess_test_data, step_split_train_data, step_train, step_predict],
-        sagemaker_session=sagemaker.Session()
+        steps=[
+            step_ingest_train_data,
+            step_preprocess_train_data,
+            step_preprocess_test_data,
+            step_split_train_data,
+            step_train,
+            step_predict,
+        ],
+        sagemaker_session=sagemaker.Session(),
     )
-    
+
     return pipeline
+
 
 if __name__ == "__main__":
     role_arn = sagemaker.get_execution_role()
